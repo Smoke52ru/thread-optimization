8a9110379d54da0845256d41fbec4b1d32b32baa
Smoke52ru <Smoke52ru@ya.ru>
2022-11-18 14:56:15 +0300

    shitty init

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// @ts-ignore
+import styles from './App.module.scss'
+
+const App = () => {
+  return (
+    <div className={styles.pageContainer}>
+      Ass
+    </div>
+  );
+};
+
+export default App;
